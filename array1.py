78fc6365852d8c7e63743dee7c090c7f4aedc1fd
Anup Jha <anup.jha6@gmail.com>
2024-01-30 23:28:04 +0530

    array creation studied

---
+++ array1.py
@@ -0,0 +1,11 @@
+import array
+
+my_array = array.array('i')
+print(my_array)
+
+
+my_array1 = array.array('i', [1, 2, 3, 4])
+print(my_array1)
+
+#my_array2 = array.array('i', [1, 2, 3, 4, "a"])
+#print(my_array2)
---
+++ array2.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+np_array = np.array([], dtype = int)
+print(np_array)
+
+
+np_array1 = np.array([1,2,3,4])
+print(np_array1)
